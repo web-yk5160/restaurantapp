77f608bd6e728d765a82166ffa8deb0f274eea57
yuta kimura <rubystudy0123@gmail.com>
2020-10-27 18:07:33 +0900

    introduced fontawesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">メイン機能</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,26 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="row text-center">
+                            <div class="col-sm-4">
+                                <a href="/management">
+                                    <h4>マネジメント</h4>
+                                    <img width="50px" src="{{ asset('images/management.svg') }}" />
+                                </a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="/cashier">
+                                    <h4>レジ / 会計</h4>
+                                    <img width="50px" src="{{ asset('images/cashier.svg') }}" />
+                                </a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="/report">
+                                    <h4>レポート</h4>
+                                    <img width="50px" src="{{ asset('images/report.svg') }}" />
+                                </a>
+                            </div>
+                    </div>
                 </div>
             </div>
         </div>
